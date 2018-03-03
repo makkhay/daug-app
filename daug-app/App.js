e5a288f087314e2b8860f027fb8908e2ecd2cc17
Prakash Gurung <makkhay@gmail.com>
2018-03-02 23:18:41 -0800

    minor bug fixes

--- daug-app/App.js
+++ daug-app/App.js
@@ -18,8 +18,8 @@ export default class App extends React.Component {
    return(
     <View style={styles.container}>
      {/* <ProfileScreen/>  */}
-    <SocialFeedScreen/> 
-    {/* <IntroScreen/> */}
+    {/* <SocialFeedScreen/>  */}
+    <IntroScreen/>
     {/* <Demo/> */}
     {/* <LoginScreen/> */}
   </View>
